8e572225e54c81680dc520f783309fd092332732
winsphinx <winsphinx@gmail.com>
2021-04-11 19:28:47 +0800

    locale zh_CN

--- covid.py
+++ covid.py
@@ -12,6 +12,42 @@ from pmdarima import arima
 from pmdarima.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
+province_name = {
+    "Anhui": "安徽",
+    "Beijing": "北京",
+    "Chongqing": "重庆",
+    "Fujian": "福建",
+    "Gansu": "甘肃",
+    "Guangdong": "广东",
+    "Guangxi": "广西",
+    "Guizhou": "贵州",
+    "Hainan": "海南",
+    "Hebei": "河北",
+    "Heilongjiang": "黑龙江",
+    "Henan": "河南",
+    "Hong Kong": "香港",
+    "Hubei": "湖北",
+    "Hunan": "湖南",
+    "Inner Mongolia": "内蒙古",
+    "Jiangsu": "江苏",
+    "Jiangxi": "江西",
+    "Jilin": "吉林",
+    "Liaoning": "辽宁",
+    "Macau": "澳门",
+    "Ningxia": "宁夏",
+    "Qinghai": "青海",
+    "Shaanxi": "陕西",
+    "Shandong": "山东",
+    "Shanghai": "上海",
+    "Shanxi": "山西",
+    "Sichuan": "四川",
+    "Tianjin": "天津",
+    "Tibet": "西藏",
+    "Xinjiang": "新疆",
+    "Yunnan": "云南",
+    "Zhejiang": "浙江",
+}
+
 
 def adjust_date(s):
     t = s.split("/")
@@ -60,11 +96,11 @@ def draw_(province, isDaily):
     plt.ticklabel_format(style='plain', axis='y')
     plt.rcParams["font.sans-serif"] = ["Microsoft YaHei"]
     if isDaily:
-        plt.title(f"每日新增预测 - {province}\nARIMA {model.model_.order}x{model.model_.seasonal_order} (R2 = {r2:.6f})")
-        plt.savefig(os.path.join("figures", f"covid-{adjust_name(province)}-daily.svg"), bbox_inches="tight")
+        plt.title(f"每日新增预测 - {province_name[province]}\nARIMA {model.model_.order}x{model.model_.seasonal_order} (R2 = {r2:.6f})")
+        plt.savefig(os.path.join("figures", f"{adjust_name(province)}-daily.svg"), bbox_inches="tight")
     else:
-        plt.title(f"累计确诊预测 - {province}\nARIMA {model.model_.order}x{model.model_.seasonal_order} (R2 = {r2:.6f})")
-        plt.savefig(os.path.join("figures", f"covid-{adjust_name(province)}.svg"), bbox_inches="tight")
+        plt.title(f"累计确诊预测 - {province_name[province]}\nARIMA {model.model_.order}x{model.model_.seasonal_order} (R2 = {r2:.6f})")
+        plt.savefig(os.path.join("figures", f"{adjust_name(province)}.svg"), bbox_inches="tight")
 
 
 if __name__ == "__main__":
@@ -85,6 +121,6 @@ if __name__ == "__main__":
         f.write("[![build status](https://github.com/winsphinx/covid-cn/actions/workflows/build.yml/badge.svg)](https://github.com/winsphinx/covid-cn/actions/workflows/build.yml)\n")
         f.write("# COVID-19 Forecasting\n\n")
         for province in provinces:
-            f.write(f"## {province}\n\n")
-            f.write(f"![img](figures/covid-{adjust_name(province)}.svg)\n\n")
-            f.write(f"![img](figures/covid-{adjust_name(province)}-daily.svg)\n\n")
+            f.write(f"## {province_name[province]}\n\n")
+            f.write(f"![img](figures/{adjust_name(province)}.svg)\n\n")
+            f.write(f"![img](figures/{adjust_name(province)}-daily.svg)\n\n")
